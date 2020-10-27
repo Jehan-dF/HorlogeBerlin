9d5622a209a9c11c3c4a14d6fbcb6c8b9cd09a33
Dorrekens <thomas.dorrekens@student.vinci.be>
2020-10-27 14:57:56 +0100

    first method + test

--- HorlogeBerlin.php
+++ HorlogeBerlin.php
@@ -1,7 +1,11 @@
 <?php
 
-
+namespace HorlogeBerlin;
 class HorlogeBerlin
 {
+    public function minutes (int $int) : int {
+        return $int%5;
+    }
+
 
 }
--- HorlogeBerlinTest.php
+++ HorlogeBerlinTest.php
@@ -1,7 +1,20 @@
 <?php
 
+require "vendor/autoload.php";
+require "HorlogeBerlin.php";
 
-class HorlogeBerlinTest extends PHPUnit_Framework_TestCase
+use HorlogeBerlin\HorlogeBerlin;
+use PHPUnit\Framework\TestCase;
+class HorlogeBerlinTest extends TestCase
 {
+    private $horlogeBerlin;
 
+    protected function setUp() : void{
+        parent::setUp();
+        $this->horlogeBerlin=new HorlogeBerlin();
+    }
+    public function test_min_given1_shouldReturn1(){
+        $actual = $this->horlogeBerlin->minutes(1);
+        $this->assertEquals(1,$actual);
+    }
 }
