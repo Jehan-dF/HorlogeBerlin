1d008812df5efe539c9cf82daf4f3851b837ac24
Jehan de Foy <jehandefoy3@gmail.com>
2020-10-27 15:19:06 +0100

    Add tests for method minutes

--- HorlogeBerlinTest.php
+++ HorlogeBerlinTest.php
@@ -13,8 +13,40 @@ class HorlogeBerlinTest extends TestCase
         parent::setUp();
         $this->horlogeBerlin=new HorlogeBerlin();
     }
+
+    //Test method minutes
+    public function test_min_given0_shouldReturn0(){
+        $actual = $this->horlogeBerlin->minutes(0);
+        $this->assertEquals(0,$actual);
+    }
+
     public function test_min_given1_shouldReturn1(){
         $actual = $this->horlogeBerlin->minutes(1);
+        $this->assertEquals(1,$actual);
+    }
+
+    public function test_min_given2_shouldReturn2(){
+        $actual = $this->horlogeBerlin->minutes(2);
+        $this->assertEquals(2,$actual);
+    }
+
+    public function test_min_given3_shouldReturn3(){
+        $actual = $this->horlogeBerlin->minutes(3);
+        $this->assertEquals(3,$actual);
+    }
+
+    public function test_min_given4_shouldReturn4(){
+        $actual = $this->horlogeBerlin->minutes(4);
+        $this->assertEquals(4,$actual);
+    }
+
+    public function test_min_given5_shouldReturn0(){
+        $actual = $this->horlogeBerlin->minutes(5);
+        $this->assertEquals(0,$actual);
+    }
+
+    public function test_min_given6_shouldReturn1(){
+        $actual = $this->horlogeBerlin->minutes(6);
         $this->assertEquals(1,$actual);
     }
 }
